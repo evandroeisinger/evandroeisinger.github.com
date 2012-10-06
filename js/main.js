c70bc13ba590b4550fce3a078125815dd31653d9
Evandro Eisinger <evandroeisinger@gmail.com>
2012-10-05 21:44:35 -0300

    Markup 3D transitions

--- js/main.js
+++ js/main.js
@@ -1,1 +1,45 @@
+var Main = (function() {
+	
+	var $container = $('.wrapper'),						
+		$left 	= $container.find('.page.left'),
+		$middle = $container.find('.page.middle'),
+		$right 	= $container.find('.page.right'),
+		$open 	= $left.find('.open'),
+		$close  = $left.find('.close'),
 
+		init = function() {
+
+			events();
+
+		},
+		events = function() {
+
+			$open.on('click', function(e) {
+
+				open();
+				return false;
+
+			});
+			$close.on('click', function(e) {
+
+				close();
+				return false;
+
+			});
+		},
+		open = function() {
+
+			$container.addClass('open');
+
+		},
+		close = function() {
+
+			$container.removeClass('open');
+
+		};
+
+	return { init : init };
+
+})();
+
+$(function() { Main.init(); });
